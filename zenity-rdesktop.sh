e4a33df1d7932f56e623473ac8aeade20ca2c306
calorique <31594913+calorique@users.noreply.github.com>
2022-04-14 19:31:21 +0800

    Update zenity-rdesktop.sh

--- zenity-rdesktop.sh
+++ zenity-rdesktop.sh
@@ -10,6 +10,7 @@
 ################ Define some functions here ##################
 ################################################################
 REMOTE_HOST_FILE="/home/local/etc/rdp_history"
+TOOLBOX=/home/lambert/exchanges
 
 function die()
 {
@@ -84,6 +85,10 @@ function check_exec()
 	fi
 }
 
+function check_cmd() {
+	[[ $# -eq 1 ]] || die "\"$*\" is more than 1 param!"
+	command -v $1 >/dev/null || die "Command \"$1\" was not found in system!"
+}
 
 function check_root_privilege()
 {
@@ -95,15 +100,61 @@ function check_root_privilege()
 
 
 function do_xfreerdp() {
-	#echo xfreerdp /cert:tofu /drive:SHARE,"$(readlink -f "${HOME}")" +window-drag /usb:id,dev:046a:00b4 /w:1440 /h:900 /v:"$1" /u:"$2" /p:"$3"
-	#xfreerdp /sec:nla +sec-ext /cert:tofu /drive:SHARE,"$(readlink -f "${HOME}")" +themes +decorations +menu-anims +window-drag /w:1440 /h:900 /v:"$1" /d:"$2" /u:"$3" /p:"$4"
+	check_cmd "xfreerdp"
+
 	# Wayland
 	#echo wlfreerdp /cert:tofu /drive:SHARE,"$(readlink -f "${HOME}")" +window-drag /w:1440 /h:900 /v:"$1" /d:"$2" /u:"$3" /p:"$4"
 	#wlfreerdp -themes /disp /cert:tofu /drive:SHARE,"$(readlink -f "${HOME}")" +window-drag /w:1440 /h:900 /v:"$1" /d:"$2" /u:"$3" /p:"$4"
-	
+
 	# Xorg
-	echo xfreerdp /sec:nla /cert:tofu /drive:SHARE,"$(readlink -f "${HOME}")" +themes +decorations +menu-anims +window-drag /w:1440 /h:900 /v:"$1" /d:"$2" /u:"$3" /p:"$4" >/tmp/xfreerdp.log
-	exec xfreerdp /sec:nla /cert:tofu /drive:SHARE,"$(readlink -f "${HOME}")" +window-drag /w:1440 /h:900 /v:"$1" /d:"$2" /u:"$3" /p:"$4"
+	#echo xfreerdp /cert:tofu /drive:SHARE,"$(readlink -f "${HOME}")" +window-drag /usb:id,dev:046a:00b4 /w:1440 /h:900 /v:"$1" /u:"$2" /p:"$3"
+	#xfreerdp /sec:nla +sec-ext /cert:tofu /drive:SHARE,"$(readlink -f "${HOME}")" +themes +decorations +menu-anims +window-drag /w:1440 /h:900 /v:"$1" /d:"$2" /u:"$3" /p:"$4"
+	#xfreerdp /cert:tofu /drive:SHARE,"$(readlink -f "${HOME}")" \
+	#	+window-drag /w:1440 /h:900 /v:"${HOST}" /u:"${USER}" /p:"${PASSWD}
+	#xfreerdp /cert:tofu /drive:SHARE,"$(readlink -f "${HOME}")" \
+	#	+window-drag /w:1440 /h:900 /v:"${HOST}" /u:"${USER}" /p:"${PASSWD}"
+	echo "Try to connect remote computer with xfreerdp: "
+	echo xfreerdp /sec:nla /cert:tofu /drive:TOOLBOX,"${TOOLBOX}" \
+		+themes +decorations +menu-anims +window-drag /w:1440 /h:900 /v:"$1" /d:"$2" /u:"$3" /p:"$4" | tee -a "/tmp/$(basename $0).log"
+
+	#https_proxy="" exec xfreerdp /sec:nla /cert:tofu /drive:SHARE,"$(readlink -f "${HOME}")" +window-drag /w:1440 /h:900 /v:"$1" /d:"$2" /u:"$3" /p:"$4"
+	https_proxy="" xfreerdp /sec:nla /cert:tofu /drive:TOOLBOX,"${TOOLBOX}" \
+		+themes +decorations +menu-anims +window-drag /w:1440 /h:900 /v:"$1" /d:"$2" /u:"$3" /p:"$4"
+	RET=$?
+
+	[[ ${RET} -eq 0 || ${RET} -eq 5 ]] && return 0
+
+	echo "Failed to connect remote computer with xfreerdp (ret=${RET}), try to reconnect with rdesktop:"
+
+	return ${RET}
+}
+
+function do_rdesktop() {
+	check_cmd "rdesktop"
+	#rdesktop -m -N -C -z -x 0x80 -a 8 -k en-us -a 16 -g 1440x900 \
+	#	-r sound:remote -r disk:RDP="$(readlink -f "${HOME}")" -r clipboard:CLIPBOARD \
+	#	"${OPTION_DOMAIN}" "${OPTION_USER}" "${OPTION_PASSWD}" "${HOST}"
+
+	#rdesktop -M -N -C -z -x 0x80 -a 8 -k en-us -a 16 -g 1440x900 \
+	#	-r sound:remote -r disk:RDP="$(readlink -f "${HOME}")" -r clipboard:CLIPBOARD \
+	#	-d "${DOMAIN}" -u "${USER}" -p "${PASSWD}" "${HOST}"
+
+	#echo "${HOST}:${DOMAIN}:${USER}:${PASSWD}" >>"${REMOTE_HOST_FILE}"
+	echo "Try to connect remote computer with rdesktop: "
+	echo rdesktop -m -N -C -z -x 0x80 -a 8 -k en-us -a 16 -g 1440x900 \
+		-r sound:remote -r disk:TOOLBOX="${TOOLBOX}" -r clipboard:CLIPBOARD \
+		-d "$2" -u "$3" -p "$4" "$1" | tee -a "/tmp/$(basename $0).log"
+
+	https_proxy="" rdesktop -m -N -C -z -x 0x80 -a 8 -k en-us -a 16 -g 1440x900 \
+		-r sound:remote -r disk:TOOLBOX="${TOOLBOX}" -r clipboard:CLIPBOARD \
+		-d "$2" -u "$3" -p "$4" "$1"
+	RET=$?
+
+	[[ ${RET} -eq 0 || ${RET} -eq 62 ]] && return 0
+
+	echo "Failed to connect remote computer with rdesktop (ret=${RET}), try to reconnect with rdesktop:"
+
+	return ${RET}
 }
 
 
@@ -157,17 +208,45 @@ function rdesk_one_string_old()
 
 	if ping -c 2 "${HOST}"
 	then
-		#rdesktop -m -N -C -z -x 0x80 -a 8 -k en-us -a 16 -g 1440x900 \
-		#	-r sound:remote -r disk:RDP="$(readlink -f "${HOME}")" -r clipboard:CLIPBOARD \
-		#	"${OPTION_DOMAIN}" "${OPTION_USER}" "${OPTION_PASSWD}" "${HOST}"
-		#xfreerdp /cert:tofu /drive:SHARE,"$(readlink -f "${HOME}")" \
-		#	+window-drag /w:1440 /h:900 /v:"${HOST}" /u:"${USER}" /p:"${PASSWD}"
-
-		do_xfreerdp "${HOST}" "${DOMAIN}" "${USER}" "${PASSWD}"
+		rdesktop_connect "${HOST}" "${DOMAIN}" "${USER}" "${PASSWD}"
 	else
 		zenity --width=180 --error --text="The host ${HOST} is unreachable!"
 		exit 2
 	fi
+}
+
+
+function add_new_session_with_vim()
+{
+	TMP_RDP_SESSION_FILE="$(mktemp --suffix=_new_rdp_session)"
+	[[ -z ${TMP_RDP_SESSION_FILE} ]] && die "add_new_session(): Failed to create tmp file!"
+	[[ -f "${TMP_RDP_SESSION_FILE}" ]] || die "add_new_session(): Failed to create tmp file!"
+
+cat >"${TMP_RDP_SESSION_FILE}" <<EOF
+Press "Shift" + "$" to input:
+host____:
+domain__:
+username:
+password:
+descript:
+EOF
+	command -v vi && EDITOR=vi
+	command -v vim && EDITOR=vim
+	command -v nvim && EDITOR=nvim
+
+	"${EDITOR}" +2 "${TMP_RDP_SESSION_FILE}"
+
+	local RHOST="$(grep 'host____' ${TMP_RDP_SESSION_FILE} | awk -F: '{print $2}')"
+	local DOMAIN="$(grep 'domain__' ${TMP_RDP_SESSION_FILE} | awk -F: '{print $2}')"
+	local USERANME="$(grep 'username' ${TMP_RDP_SESSION_FILE} | awk -F: '{print $2}')"
+	local PASSWORD="$(grep 'password' ${TMP_RDP_SESSION_FILE} | awk -F: '{print $2}')"
+	local DESCRIPTION="$(grep 'descript' ${TMP_RDP_SESSION_FILE} | sed 's/\s\+/_/g' | awk -F: '{print $2}')"
+
+	rm -fv "${TMP_RDP_SESSION_FILE}"
+
+	echo "${RHOST}:${DOMAIN}:${USERANME}:${PASSWORD}:${DESCRIPTION}" | tee -a "${REMOTE_HOST_FILE}"
+
+	exec "$0"
 }
 
 
@@ -186,7 +265,7 @@ function rdesk_one_string()
 	HOST=$(echo "${INSTR}" | awk -F: '{print $1}')
 	if [[ -z ${HOST} ]]
 	then
-		warn "Without host."
+		warn "Param host is Null."
 	else
 		local DOMAIN
 		DOMAIN=$(echo "${INSTR}" | awk -F: '{print $2}')
@@ -217,14 +296,7 @@ function rdesk_one_string()
 
 	if ping -c 2 "${HOST}"
 	then
-		#rdesktop -m -N -C -z -x 0x80 -a 8 -k en-us -a 16 -g 1440x900 \
-		#	-r sound:remote -r disk:RDP="$(readlink -f "${HOME}")" -r clipboard:CLIPBOARD \
-		#	"${OPTION_DOMAIN}" "${OPTION_USER}" "${OPTION_PASSWD}" "${HOST}"
-		#xfreerdp /cert:tofu /drive:SHARE,"$(readlink -f "${HOME}")" \
-		#	+window-drag /w:1440 /h:900 /v:"${HOST}" /u:"${USER}" /p:"${PASSWD}"
-		#echo "${HOST}:${DOMAIN}:${USER}:${PASSWD}" >>"${REMOTE_HOST_FILE}"
-		echo "${INSTR}" >>"${REMOTE_HOST_FILE}"
-		do_xfreerdp "${HOST}" "${DOMAIN}" "${USER}" "${PASSWD}"
+		rdesktop_connect "${HOST}" "${DOMAIN}" "${USER}" "${PASSWD}"
 	else
 		zenity --width=180 --error --text="The host ${HOST} is unreachable!"
 		exit 2
@@ -232,7 +304,7 @@ function rdesk_one_string()
 }
 
 
-function rdesk_one_by_one()
+function new_session_with_zenity()
 {
 	local INSTR
 	INSTR=$(zenity --forms --title="Remote Desktop" \
@@ -241,7 +313,8 @@ function rdesk_one_by_one()
 		--add-entry="host" \
 		--add-entry="domain" \
 		--add-entry="user" \
-		--add-password="password")
+		--add-password="password" \
+		--add-entry="descript")
 
 	[[ -z ${INSTR} ]] && die "Null param input!"
 
@@ -253,6 +326,8 @@ function rdesk_one_by_one()
 	USER=$(echo "${INSTR}" | awk -F',' '{print $3}')
 	local PASSWD
 	PASSWD=$(echo "${INSTR}" | awk -F',' '{print $4}')
+	local DESC
+	DESC=$(echo "${INSTR}" | awk -F',' '{print $5}')
 
 	[[ 'X******' == "X${PASSWD}" ]] && PASSWD=Arety2018
 	if [[ -z ${HOST} ]]
@@ -271,50 +346,56 @@ function rdesk_one_by_one()
 	[[ -z ${USER} ]] || OPTION_USER="-u ${USER}"
 	[[ -z ${PASSWD} ]] || OPTION_PASSWD="-p ${PASSWD}"
 
-	if ping -c 2 "${HOST}"
-	then
-		#rdesktop -m -N -C -z -x 0x80 -a 8 -k en-us -a 16 -g 1440x900 \
-		#	-r sound:remote -r disk:RDP="$(readlink -f "${HOME}")" -r clipboard:CLIPBOARD \
-		#	"${OPTION_DOMAIN}" "${OPTION_USER}" "${OPTION_PASSWD}" "${HOST}"
+	echo "${HOST}:${DOMAIN}:${USER}:${PASSWD}:${DESC}" | tee -a "${REMOTE_HOST_FILE}" \
+		|| die "Failed to create new session!"
 
-		#xfreerdp /cert:tofu /drive:SHARE,"$(readlink -f "${HOME}")" \
-		#	+window-drag /w:1440 /h:900 /v:"${HOST}" /u:"${USER}" /p:"${PASSWD}
-		
-		do_xfreerdp "${HOST}" "${DOMAIN}" "${USER}" "${PASSWD}"
-	else
-		zenity --width=180 --error --text="The host ${HOST} is unreachable!"
-		exit 2
-	fi
+	rdesktop_connect "${HOST}:${DOMAIN}:${USER}:${PASSWD}" || {
+		echo "rdesktop_connectr returned $?"
+		default_edit "${REMOTE_HOST_FILE}"
+		exec $0 "$@"
+	}
 }
 
 
-function edit_menu()
-{
-	if [[ -x /usr/bin/subl ]]
-	then
-		/usr/bin/subl "$0"
-	elif [[ -x /usr/bin/gedit ]]
-	then
-		/usr/bin/gedit "$0"
-	elif [[ -x /usr/bin/nvim ]]
-	then
-		/usr/bin/nvim "$0"
-	fi
+function default_edit() {
+	command -v vi >/dev/null && EDITOR=vi
+	command -v vim >/dev/null && EDITOR=vim
+	command -v nvim >/dev/null && EDITOR=nvim
+	command -v gedit >/dev/null && EDITOR=gedit
+	#command -v subl && EDITOR=subl
+
+	"${EDITOR}" "$@"
 }
 
 
-function edit_connection_list()
-{
-	if [[ -x /usr/bin/subl ]]
-	then
-		/usr/bin/subl "${REMOTE_HOST_FILE}"
-	elif [[ -x /usr/bin/gedit ]]
-	then
-		/usr/bin/gedit "${REMOTE_HOST_FILE}"
-	elif [[ -x /usr/bin/nvim ]]
-	then
-		/usr/bin/nvim "${REMOTE_HOST_FILE}"
-	fi
+function zenity_edit() {
+	OPTION=$(zenity --width=250 --height=210 \
+		--list --title "Zenity Select Editor Dialog" \
+		--column "" \
+		--hide-header \
+		"sublime_text" \
+		"gedit" \
+		"neovim" \
+		"[Quit]")
+
+	EDITOR=vim
+	case ${OPTION} in
+		"sublime_text")
+			EDITOR=subl;;
+		"gedit")
+			EDITOR=gedit;;
+		"neovim")
+			EDITOR=nvim;;
+		"[Quit]")
+			exit 0;;
+	esac
+
+	command -v "${EDITOR}" >/dev/null || {
+		zenity --warning --width=200 --text "${EDITOR} was not found, try other editor!"
+		zenity_edit "$@"
+	}
+
+	"${EDITOR}" "$@"
 }
 
 
@@ -326,7 +407,7 @@ function rdesktop_connect()
 	[[ -z "${HOST}" ]] && die "HOST is Null!"
 
 	DOMAIN="$(echo "$1" | awk -F: '{print $2}')"
-	[[ -z "${DOMAIN}" ]] && die "DOMAIN is Null!"
+	#[[ -z "${DOMAIN}" ]] && die "DOMAIN is Null!"
 
 	USER="$(echo "$1" | awk -F: '{print $3}')"
 	[[ -z "${USER}" ]] && die "USER is Null!"
@@ -336,12 +417,8 @@ function rdesktop_connect()
 
 	if ping -c 2 "${HOST}"
 	then
-		#rdesktop -M -N -C -z -x 0x80 -a 8 -k en-us -a 16 -g 1440x900 \
-		#	-r sound:remote -r disk:RDP="$(readlink -f "${HOME}")" -r clipboard:CLIPBOARD \
-		#	-d "${DOMAIN}" -u "${USER}" -p "${PASSWD}" "${HOST}"
-		#xfreerdp /cert:tofu /drive:SHARE,"$(readlink -f "${HOME}")" \
-		#	+window-drag /w:1440 /h:900 /v:"${HOST}" /u:"${USER}" /p:"${PASSWD}"
-		do_xfreerdp "${HOST}" "${DOMAIN}" "${USER}" "${PASSWD}"
+		do_xfreerdp "${HOST}" "${DOMAIN}" "${USER}" "${PASSWD}" || do_rdesktop "${HOST}" "${DOMAIN}" "${USER}" "${PASSWD}"
+		echo "All done!"
 	else
 		zenity --width=180 --error --text="The host ${HOST} is unreachable!"
 		exit 2
@@ -349,15 +426,13 @@ function rdesktop_connect()
 }
 
 # Main Process
-#check_exec /usr/bin/rdesktop
-check_exec /usr/bin/xfreerdp
-check_exec /usr/bin/zenity
+check_cmd "zenity"
 
 [[ -f "${REMOTE_HOST_FILE}" ]] || {
 	touch "${REMOTE_HOST_FILE}" || die "Failed to create ${REMOTE_HOST_FILE}!"
 }
 
-unset https_proxy
+#unset https_proxy
 
 # Debug:
 #cat "${REMOTE_HOST_FILE}" | while read X; do
@@ -366,7 +441,7 @@ unset https_proxy
 
 #		--column "Host:Domain:User:Passwd" \
 #		--column "--------------------------------------------------" \
-OPTN=$(zenity --width=500 --height=400 \
+OPTN=$(zenity --width=700 --height=600 \
 		--list --title "Zenity Remote Desktop Dialog" \
 		--column "" \
 		--hide-header \
@@ -377,22 +452,34 @@ OPTN=$(zenity --width=500 --height=400 \
 		"[Create new remote connection...]" \
 		"[Edit connection list...]" \
 		"[Edit this menu...]" \
-)
+		"[Quit]")
 
 #zenity --width=500 --info --text="Confirm selection: ${OPTN}"
 
 case ${OPTN} in
-	"[Create new remote connection...]" |	"Manual input: one string")
-		rdesk_one_string;;
-	"Manual input: one by one")
-		rdesk_one_by_one;;
+	"[Create new remote connection...]")
+		#rdesk_one_string
+		#rdesk_one_by_one
+		#add_new_session_with_vim
+		new_session_with_zenity
+		;;
 	"[Edit connection list...]")
-		edit_connection_list;;
+		#zenity_edit "${REMOTE_HOST_FILE}"
+		default_edit "${REMOTE_HOST_FILE}"
+		exec $0 "$@"
+		;;
 	"[Edit this menu...]")
-		edit_menu;;
+		#zenity_edit "$0"
+		default_edit "$0"
+		exec $0 "$@"
+		;;
+	"[Quit]")
+		exit 0
+		;;
 	*)
 		#zenity --width=120 --error --text="Invalid option!";;
-		rdesktop_connect "${OPTN}";;
+		rdesktop_connect "${OPTN}"
+		;;
 esac
 
 # xfreerdp /cert:tofu /drive:SHARE,"$(readlink -f "${HOME}")" +window-drag /w:1440 /h:900 /v:"${HOST}" /u:"${USER}" /p:"${PASSWD}"
